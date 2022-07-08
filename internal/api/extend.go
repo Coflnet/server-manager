413cc714e5317a830c8c5ae1480932b1d2cc2cfe
Florian Mühlhans <muehlhans.florian@web.de>
2022-07-08 22:13:06 +0200

    add the extend stuff

--- internal/api/extend.go
+++ internal/api/extend.go
@@ -2,9 +2,45 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"server-manager/internal/mongo"
+	"server-manager/internal/usecase"
+	"time"
 )
 
+type ServerExtensionRequest struct {
+	Name          string `json:"name"`
+	ExtendSeconds int    `json:"extendSeconds"`
+}
+
 func extendServer(c *gin.Context) {
-	c.Status(http.StatusNotImplemented)
+
+	var request ServerExtensionRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	duration := time.Duration(request.ExtendSeconds) * time.Second
+
+	server, err := mongo.ServerByName(request.Name)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = usecase.ExtendServer(server, duration)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "server extended",
+	})
 }
