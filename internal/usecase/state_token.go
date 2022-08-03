f3b7141f50de6d88d9c891fffc6c4cb9f7ab8934
Florian Mühlhans <muehlhans.florian@web.de>
2022-08-03 19:59:30 +0200

    add log line

--- internal/usecase/state_token.go
+++ internal/usecase/state_token.go
@@ -26,16 +26,20 @@ func CreateStateTransferToken() (string, error) {
 		log.Error().Err(err).Msgf("error requesting a new sniper transfer token")
 	}
 
-	str, err := ioutil.ReadAll(resp.Body)
+	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Panic().Err(err).Msgf("somehow body could not been read")
 	}
+
+	str := string(b)
 
 	if resp.StatusCode != http.StatusOK {
 		log.Error().Msgf("error requesting a new sniper transfer token, status code %d\n%s", resp.StatusCode, str)
 	}
 
-	return string(str), nil
+	log.Info().Msgf("received a new sniper transfer token: %s, status code: %v, body: %v", str, resp.Status, b)
+
+	return str, nil
 }
 
 func sniperUrl() string {
